Fix exptime calculation (multiple exposures, not just one long one)

exptime.py
@@ -19,7 +19,6 @@ Guesses at exposure time based on saturating the detector, then scales with t^-0
 beatty = np.genfromtxt("table1.dat", dtype=None)
 beatty.dtype.names = ('Angstrom', 'Teff', 'Uncertaintykms')
 
-
 def λ_peak(Teff, λ_min=0 * u.angstrom, λ_max=1e13 * u.angstrom):
 	λ = (c.b_wien / (Teff * u.K)).si # Star is assumed to be a perfect blackbody
 	# Correcting for if peak wavelength is outside of detector, angstroms assumed
@@ -35,6 +34,7 @@ def extinction(λ):
 	# http://www.gemini.edu/sciops/telescopes-and-sites/observing-condition-constraints/extinction
 	# http://adsabs.harvard.edu/abs/1994IAPPP..57...12S
 	# Values should not be considered trustworthy outside of ~3000-10000 Angstroms, and really 3500-9500 at that.
+	# Rayleigh scattering, and a general scattering absorption are considered, but the Water vapor, etc lines in the IR are not!
 	return (0.09 + (3080.0/λ)**4)
 
 def airmass(zenith_angle, site_elevation=0, scale_height=8400):
@@ -47,28 +47,22 @@ def time_guess(Teff, FeH, logg, vsini, theta_rot, rstar, dstar, v_mac, atmo, eff
 	#print("Teff:", Teff, " BT-Settl:", round(Teff,-2), " Beatty RV:", round(Teff/200)*200)
 	
 	# BT Settl spectra are labled by Teff, and available every 100 K.
-	# These spectra have a wavelength (Angstroms), and a flux (1e8 erg/s/cm^2/Angstrom) column
+	# These spectra have a wavelength (Angstroms), and a flux (1 erg/s/cm^2/Angstrom) column
 	# sum of flux(λ)*Δλ(λ)/1e8 == total flux (in power/area) emitted. 
 	# Multiply bt stellar_radius^2/distance^2 for recieved.
 	# Spectra downloaded from other sources will use different units!
-	
-	#De-duping, this is not necessary if spectra are downloaded from another source.
-	model = [np.array([0,0])]
-	for x in BTSettl:
-		if np.array_equal(x,model[-1]) == False:
-			model.append(x)
 	photons = 0
 	#Counting up photons for SNR -> exposure time calc
 	
 	#adding up photons in 100 A chunk centered on peak wavelength:
-	for i in np.arange(0, len(model)-1):
-		if((model[i][0]>= λ_peak/u.angstrom-50) and (model[i][0] <= λ_peak/u.angstrom+15)):
-			power = (model[i][1]*1e-8*u.erg/u.cm**2/u.s/u.angstrom) * ((model[i+1][0]-model[i][0]) * u.angstrom)
+	for i in np.arange(0, len(BTSettl)-1):
+		if((BTSettl[i][0]>= λ_peak/u.angstrom-50) and (BTSettl[i][0] <= λ_peak/u.angstrom+15)):
+			power = (BTSettl[i][1]*u.erg/u.cm**2/u.s/u.angstrom) * ((BTSettl[i+1][0]-BTSettl[i][0]) * u.angstrom)
 			power *= rstar**2/dstar**2 #rescaling emitted spectrum based on stellar surface area and distance from us
-			opacity = np.exp(-extinction(model[i][0]) * atmo)
+			opacity = np.exp(-extinction(BTSettl[i][0]) * atmo)
 			power *= opacity #rescaling because of atmospheric scattering/absorption.
 			power = power.si
-			photons += (power * model[i][0] * u.angstrom / (c.h * c.c)) * area * efficiency
+			photons += (power * BTSettl[i][0] * u.angstrom / (c.h * c.c)) * area * efficiency
 	photons /= (100*u.angstrom / (λ_peak/R)) #100 Angstrom range -> per resolution element
 	photons /= n_pix #per resolution element -> per pixel
 	#print("photons/s/pixel", photons)
@@ -79,11 +73,13 @@ def time_guess(Teff, FeH, logg, vsini, theta_rot, rstar, dstar, v_mac, atmo, eff
 def time_actual(sigma_v, Teff, FeH, logg, vsini, theta_rot, rstar, dstar, v_mac, atmo, exptime, efficiency, area, R, gain, read_noise, dark_current, n_pix, λ_min, λ_max, Δλ, read_time):
 	rv_actual = rvrms.rvcalc(Teff, FeH, logg, vsini, theta_rot, rstar, dstar, v_mac, atmo, exptime, efficiency, area, R, gain, read_noise, dark_current, n_pix, λ_min, λ_max, Δλ)
 	time_actual = exptime * (rv_actual/sigma_v)**2
-	reads = read_time * np.ceil(time_actual/exptime)
-	return time_actual, reads
+	number_exposures = np.ceil(time_actual/exptime)
+	time_exposure = time_actual/number_exposures
+	reads = read_time * number_exposures
+	return time_actual, reads, time_exposure, number_exposures
 
 if __name__ == '__main__':
-	# HARPS
+	# HARPS / ESO 3.6 m at La Silla
 	telescope = 3.566 * u.m
 	#area = np.pi * (telescope/2)**2 # telescope area, central obstruction ignored
 	area = 8.8564 * u.m * u.m
@@ -108,19 +104,20 @@ if __name__ == '__main__':
 	dstar = 1.3 * u.pc
 	vsini = 2*np.pi*rstar/(38.7*u.day) * u.s/u.km
 	theta_rot = 1.13*vsini.si
-	v_mac = 2.0 # km/s, best guess
+	v_mac = np.float('nan') # km/s, but exact value unclear
 	
 	sigma_v = 5e-5 # target single measurement photon noise precision in km/s
 	
 	# Atmo
 	zenith_angle = 0 # Can be read in from stellar observations!
 	scale_height = 8400 * u.m #Can make arguments for `8500 m to ~7500 m, but the higher temperatures typical of the lower atmosphere are more relevant here.
-	site_elevation = 2016 * u.m #Kitt Peak/WIYN
+	site_elevation = 2400 * u.m #ESO 3.6 m telescope, La Silla, Chile
 	atmo = airmass(zenith_angle, site_elevation, scale_height)
 
 	λ_peak = λ_peak(Teff, λ_min, λ_max)
 	#BeattyWaves = np.arange(λ_min/u.angstrom, λ_max/u.angstrom, Δλ/u.angstrom)
-	guess = time_guess(Teff, FeH, logg, vsini, theta_rot, rstar, dstar, atmo, efficiency, area, R, gain, read_noise, dark_current, n_pix, λ_peak, well_depth)
-	actual, readout = time_actual(sigma_v, Teff, FeH, logg, vsini, theta_rot, rstar, dstar, atmo, guess, efficiency, area, R, gain, read_noise, dark_current, n_pix, λ_min, λ_max, Δλ, read_time)
-	print("guess, actual exposure, readout(s), total")
-	print(guess, actual, readout, actual+readout)
+	print("Exposure time test ("+str(sigma_v)+" km/s target precision) with HARPS on Alpha Cen B. 76 seconds on-sky expected.")
+	guess = time_guess(Teff, FeH, logg, vsini, theta_rot, rstar, dstar, v_mac, atmo, efficiency, area, R, gain, read_noise, dark_current, n_pix, λ_peak, well_depth)
+	actual, readout, exposure, number  = time_actual(sigma_v, Teff, FeH, logg, vsini, theta_rot, rstar, dstar, v_mac, atmo, guess, efficiency, area, R, gain, read_noise, dark_current, n_pix, λ_min, λ_max, Δλ, read_time)
+	print("guess, actual exposure, readout(s), total, 'real' individual exposure, number of exposures")
+	print(guess, actual, readout, actual+readout, exposure, number)

exptime_batch.py
@@ -16,17 +16,17 @@ sim = simulation.simulation('simulation.ini')
 Δλ = 100 * u.angstrom
 λ_max = sim.instruments[0].λmax * u.angstrom
 λ_min = sim.instruments[0].λmin * u.angstrom
-sigma_v = 3e-5 # km/s
+sigma_v = 5e-5 # km/s
 zenith_angle = 10*np.pi/180 #obviously should be set based on typical object altitude
 atmo = exptime.airmass(zenith_angle,sim.elevation,8400)
 area = sim.telescopes[0].area * u.m * u.m
 
 starlist = np.genfromtxt("targetstars.csv", delimiter=",", dtype=None, names=True)
-eta_list = open("eta_lbt_ilocator_3cms.txt", 'w')
+eta_list = open("eta_list.txt", 'w')
 eta_list.write("Automatically generated list by exptime_batch.py on "+now+"\n\n")
 eta_list.write(" IdentList\n----------\n\n")
-eta_list.write(" # \ttyped ident\t  coord1 (ICRS,J2000/2000)     \tMag V\t      spec type  \tExp Time\n")
-eta_list.write("--- ----------- -----------------------------   ------  --------------- -------------\n")
+eta_list.write(" # \ttyped ident\t  coord1 (ICRS,J2000/2000)     \tMag V\t      spec type  \tExp Time\tSky Time\tSingle Exposure\tNumber of Exposures\n")
+eta_list.write("--- ----------- -----------------------------   ------  --------------- ------------- ------- ---- --\n")
 for star in starlist:
     Teff = star['K']
     λ_peak = exptime.λ_peak(Teff, λ_min, λ_max)
@@ -40,7 +40,7 @@ for star in starlist:
     dark_current = sim.instruments[0].dark_current / u.hour
     read_time = sim.instruments[0].read_time * u.s
     guess = exptime.time_guess(Teff, FeH, logg, vsini, theta_rot, rstar, dstar, vmac, atmo, sim.instruments[0].efficiency, area, sim.instruments[0].R, sim.instruments[0].gain, sim.instruments[0].read_noise, dark_current, sim.instruments[0].n_pix, λ_peak, sim.instruments[0].well_depth)
-    actual, readout = exptime.time_actual(sigma_v, Teff, FeH, logg, vsini, theta_rot, rstar, dstar, vmac, atmo, guess, sim.instruments[0].efficiency, area, sim.instruments[0].R, sim.instruments[0].gain, sim.instruments[0].read_noise, dark_current, sim.instruments[0].n_pix, λ_min, λ_max, Δλ, read_time)
+    actual, readout, exposure, number = exptime.time_actual(sigma_v, Teff, FeH, logg, vsini, theta_rot, rstar, dstar, vmac, atmo, guess, sim.instruments[0].efficiency, area, sim.instruments[0].R, sim.instruments[0].gain, sim.instruments[0].read_noise, dark_current, sim.instruments[0].n_pix, λ_min, λ_max, Δλ, read_time)
     # We don't know if we're getting decimal degrees or sexigesimal formatted coordinates
     try:
         RADEC = str(star['hms'])[2:-1]+' '+str(star['dms'])[2:-1]
@@ -50,7 +50,7 @@ for star in starlist:
     MK = str(star['MK'])[2:-1] #Spectra type is nice to have
     if MK == "":
         MK = "XXX"
-    line = '0'+'\t'+str(star['Name'])[2:-1]+'\t'+RADEC+'\t'+str(star['mag'])+"\t"+MK+"\t"+str((actual+readout).to(u.minute).value)+'\t'+str(actual.to(u.minute).value)+'\n'
+    line = '0'+'\t'+str(star['Name'])[2:-1]+'\t'+RADEC+'\t'+str(star['mag'])+"\t"+MK+"\t"+str((actual+readout).to(u.minute).value)+'\t'+str(actual.to(u.minute).value)+'\t'+str(exposure.to(u.minute).value)+'\t'+str(int(number))+'\n'
     print(line)
     eta_list.write(line)
     #print("guess, actual exposure, readout(s), total")

rvrms.py
@@ -18,7 +18,7 @@ def extinction(λ):
 	# Values should not be considered trustworthy outside of ~3000-10000 Angstroms, and really 3500-9500 at that.
 	return (0.09 + (3080.0/λ)**4)
 
-def rvcalc(Teff, FeH, logg, vsini, theta_rot, rstar, dstar, v_mac, airmass, exptime, efficiency, area, R, gain, read_noise, dark_current, n_pix, λmin, λmax, dλ):
+def rvcalc(Teff, FeH, logg, vsini, theta_rot, rstar, dstar, v_mac, airmass, exptime, efficiency, area, R, gain, read_noise, dark_current, n_pix, λmin, λmax, dλ, n_expose):
 	# Need to fix to make less unit dependent
 	λ_max = λmax.to(u.angstrom).value
 	λ_min = λmin.to(u.angstrom).value
@@ -64,7 +64,7 @@ def rvcalc(Teff, FeH, logg, vsini, theta_rot, rstar, dstar, v_mac, airmass, expt
 	for λ in np.arange(0, len(BeattyWaves)):
 		pix = n_pix*Δλ*R/I_0[λ][0]
 		# For now, assume equal signal per pixel. A gaussian would be better.
-		I_0[λ][3] = I_0[λ][2]/pix*gain / np.sqrt(I_0[λ][2]/pix*gain + (gain*read_noise*2.2*2)**2 + (gain*dark_current*exptime)**2)
+		I_0[λ][3] = I_0[λ][2]/pix*gain / np.sqrt(I_0[λ][2]/pix*gain + (gain*read_noise*2.2*2*n_expose)**2 + (gain*dark_current*exptime)**2)
 		I_0[λ][4] = I_0[λ][3]**2 * pix / ((c.c/(u.km/u.s)).si * Δλ/I_0[λ][0])
 	
 	#print(I_0)
@@ -161,7 +161,7 @@ if __name__ == '__main__':
 			for obs in starobs:
 		# get v_rms for every observation
 	'''
-	site_elevation = 2016.0 # m
+	site_elevation = 2400.0 # m
 
 	# Beatty spectra (simulated): 100 A chunks; Solar metallicity;
 	# R = 100k (4000-6500 A), 165000 (6500-10000 A), 88000 (10000A - 25000 A)
@@ -198,7 +198,7 @@ if __name__ == '__main__':
 	theta_rot = 1.13*vsini.si # Rough approximation, but rotational effects are near-linear, no matter limb-darkening.
 	vmac = float('nan') #actual value uncertain in my sources.
 	
-	# HARPS instrument on ??? telescope (nominally in La Silla)
+	# HARPS instrument on ESO 3.6 m telescope (in La Silla)
 	telescope = 3.566 * u.m #diameter
 	#area = np.pi * (telescope/2)**2 # telescope area, central obstruction ignored
 	area = 8.8564 * u.m * u.m
@@ -215,6 +215,12 @@ if __name__ == '__main__':
 	well_depth = 30000 #electrons (or photons)
 	read_time = 10 * u.s
 	exptime = 76 * u.s
+	exposures = 1
 
-	print("Radial Velocity test with HARPS on Alpha Cen B. Expected precision 5 cm/s (5e-5 km/s):")
-	print(rvcalc(Teff, FeH, logg, vsini, theta_rot, rstar, dstar, vmac, airmass, exptime, efficiency, area, R, gain, read_noise, dark_current, n_pix, λ_min, λ_max, Δλ))
+	print("Radial Velocity test ("+str(exptime)+" exposure) with HARPS on Alpha Cen B. Expected precision 5 cm/s (5e-5 km/s):")
+	print(rvcalc(Teff, FeH, logg, vsini, theta_rot, rstar, dstar, vmac, airmass, exptime, efficiency, area, R, gain, read_noise, dark_current, n_pix, λ_min, λ_max, Δλ, exposures))
+
+	exptime = 7 * u.s
+	exposures = 11
+	print("Radial Velocity test ("+str(exposures)+"x "+str(exptime)+" exposures) with HARPS on Alpha Cen B. Expected precision 5 cm/s (5e-5 km/s):")
+	print(rvcalc(Teff, FeH, logg, vsini, theta_rot, rstar, dstar, vmac, airmass, exptime, efficiency, area, R, gain, read_noise, dark_current, n_pix, λ_min, λ_max, Δλ, 1)/np.sqrt(exposures))

rvrms_batch.py
@@ -37,7 +37,7 @@ for target in target_list:
 			# if we have actual observations, not just the test/setup one, we can calculate RVs
 			print(target['Name'])
 			star_rms = open(simpath+target['Name']+"_rv.txt", 'w')
-			star_rms.write("obs_start,obs_end,duration,altitude,azimuth,photonprec,instprec,rvprec,rvmeas\n")
+			star_rms.write("obs_start,obs_end,duration,altitude,azimuth,exposures,photonprec,instprec,rvprec,rvmeas\n")
 			Teff = target['K']
 			FeH = target['Sun']
 			logg = target['cms']
@@ -47,16 +47,17 @@ for target in target_list:
 			dstar = target['pc'] * u.pc
 			vmac = target['Vmac']
 			for i in star:
-				exptime = i['duration'] * u.s
+				exptime = i['duration'] * u.minute
 				zenith_angle = (90-i['altitude'])*np.pi/180.0
 				airmass = np.exp(-sim.elevation/8400)/np.cos(zenith_angle)
-				photonrms = 1000*rvrms.rvcalc(Teff, FeH, logg, vsini, theta_rot, rstar, dstar, vmac, airmass, exptime, sim.instruments[0].efficiency, area, sim.instruments[0].R, sim.instruments[0].gain, sim.instruments[0].read_noise, dark_current, sim.instruments[0].n_pix, λ_min, λ_max, Δλ)
+				n_expose = i['exposures']
+				photonrms = 1000*rvrms.rvcalc(Teff, FeH, logg, vsini, theta_rot, rstar, dstar, vmac, airmass, exptime, sim.instruments[0].efficiency, area, sim.instruments[0].R, sim.instruments[0].gain, sim.instruments[0].read_noise, dark_current, sim.instruments[0].n_pix, λ_min, λ_max, Δλ, n_expose)
 				vrms = np.sqrt(photonrms**2+instrms**2)
 				vmeas = np.random.normal(0.0, vrms)
-				line = str(i['obs_start'])+","+str(i['obs_end'])+","+str(i['duration'])+","+str(i['altitude'])+","+str(i['azimuth'])+","+str(photonrms)+","+str(instrms)+","+str(vrms)+","+str(vmeas)+"\n"
+				line = str(i['obs_start'])+","+str(i['obs_end'])+","+str(i['duration'])+","+str(i['altitude'])+","+str(i['azimuth'])+","+str(i['exposures'])+","+str(photonrms)+","+str(instrms)+","+str(vrms)+","+str(vmeas)+"\n"
 				star_rms.write(line)
 				print(vrms, vmeas)
-		star_rms.close()
+			star_rms.close()
 
 '''
 # How the other batch file was going to grab star info